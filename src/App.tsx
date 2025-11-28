5a7a7fa1f3ad2e91a68d2b0602b1636de8a2b964
Mehrad Farahnak <mehradfarahnak@Mehrads-MacBook-Air.local>
2025-11-28 19:24:33 +0330

    hero section animation done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Hero from "./components/Hero.tsx";
 
 const App = () => {
-    return (
-        <main className='relative min-h-screen w-screen overflow-x-hidden' >
-            <Hero/>
-        </main>
-    )
-}
-export default App
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+      <section className="z-0 min-h-screen bg-blue-500" />
+    </main>
+  );
+};
+export default App;
